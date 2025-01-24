3d132374fd1be84851127ac1e0080e25074cb12b
cyberneel <neelesh.chevuri25@gmail.com>
2025-01-24 11:33:53 -0600

    Reformated solutions cards and added latest winners

--- src/components/SolutionCard.jsx
+++ src/components/SolutionCard.jsx
@@ -6,20 +6,24 @@ import './SolutionCard.css';
 const SolutionCard = (props) => {
     const {solutionName, solutionAuthors, solutionSchool, solutionLink} = props;
 
+    const place = solutionName.includes('1st Place') ? '1st' : solutionName.includes('2nd Place') ? '2nd' : solutionName.includes('3rd Place') ? '3rd' : '';
+
 
   return (
     <div className="solution-card">
         <div className="solution-card__left">
-          <img src={`/school-icons/${solutionSchool.split(' ').join('')}.png`} alt="school img" />
+          { solutionSchool && <img src={`/school-icons/${solutionSchool.split(' ').join('')}.png`} alt="school img" /> }
+          { place && <img src={`/school-icons/${place}.webp`} alt="school img" /> }
         </div>
         <div className="solution-card__mid">
           <h2 className="solution-card__solution-name">{solutionName}</h2>
           <h3 className="solution-card__solution-authors">{solutionAuthors}</h3>
-          <p className="solution-card__solution-school">@ {solutionSchool}</p>
+          {solutionSchool && <p className="solution-card__solution-school">@ {solutionSchool}</p>}
         </div>
-        <div className="solution-card__right">
+        { solutionLink != "#" && <div className="solution-card__right">
           <Link to={solutionLink} className='link-btn'>View Solution</Link>
         </div>
+        }
     </div>
   )
 }
